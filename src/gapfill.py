2e073c1845e62d9a6cafb10892e98981c9717293
jo <jotech@users.noreply.github.com>
2018-01-31 19:40:01 +0100

    scripts for topological gapfilling
    - candidate reactions found from pathway blast can be used as reference
    database in gapfilling synthesis of biomass components and carbon source
    usage
    - uses gapfilling approach of cobrapy

---
+++ src/gapfill.py
@@ -0,0 +1,142 @@
+from cobra.flux_analysis import gapfill
+from cobra.flux_analysis.gapfilling import GapFiller
+from cobra.io import read_sbml_model
+from cobra.io import write_sbml_model
+#from cobra import Model, Reaction, Metabolite
+#import pandas
+import sys
+import re
+import os
+import gapfill_reference
+
+if len(sys.argv) != 3:
+  print("usage:", sys.argv[0], "file.xml reaction.lst")
+  sys.exit(0)
+
+#seedrDB = pandas.read_csv("/home/jo/uni/gapseq/dat/seed_reactions.tsv", sep="\t")
+#seedmDB = pandas.read_csv("/home/jo/uni/gapseq/dat/seed_metabolites.tsv", sep="\t")
+
+mod = repair_mass_balance(read_sbml_model(sys.argv[1]), verbose=False)
+with open(sys.argv[2]) as f:
+    lines = f.read()
+newR = lines.rstrip("\n").split(" ") # remove linebreak at file end
+
+#
+# Create reference model with reactions pool
+#
+
+refmod = get_reference(mod, newR, delete_unbalanced=True, verbose=False)
+
+
+# CORRECTION IN SEED DATABASE
+if "rxn05115_c0" in refmod.reactions:
+    refmod.reactions.rxn05115_c0.upper_bound=1000 # wrong direction
+if "rxn03031_c0" in mod.reactions:
+    mod.reactions.rxn03031_c0.lower_bound=-1000 # bidirectional http://www.rhea-db.org/reaction?id=17325
+
+
+ignore = ["H", "Li", "Na", "Mg", "K", "Ca", "P", "Fe", "Cl", "Zn", "Mn", "Mo", "Se", "Co", "Cu", "Ni", "W", "H2O"]
+# prepare model with minimal medium
+minmed = ["cpd00027", "cpd00007", "cpd00001", # glc, o2, h2o
+          #"cpd00020", # pyruvate
+          "cpd00106", # fumarat # myb10
+          "cpd00013", # nh3
+          "cpd00048", # sulfate
+          # minerals
+          "cpd00009", "cpd00012", "cpd00030", "cpd00034", "cpd00058", "cpd00063", "cpd00067", "cpd00099", "cpd00149", "cpd00205", "cpd00254", "cpd10515", "cpd00971", "cpd01012", "cpd10516", "cpd11574"]
+
+def set_medium(model, medium):
+    mod = model.copy()
+    for ex in mod.exchanges:
+        ex.lower_bound=0
+
+    for m in medium: # set medium
+        #ex = "EX_"+m+"_LPAREN_e_RPAREN_" # vmh
+        ex = "EX_"+m+"_e0"
+        #ex = m
+        if ex in mod.reactions:
+            mod.reactions.get_by_id(ex).lower_bound=-1000
+        else:
+            print "compound not found in model:", m
+    return(mod)
+
+def checkProduction(model, metabolite):
+    modtmp = set_medium(model, minmed)
+    #refmod.reactions.rxn05115.upper_bound=1000
+    #modnew.add_reactions([refmod.reactions.rxn00533])
+
+    newM = []
+    for m in refmod.metabolites:
+        if m not in modtmp.metabolites:
+            newM.append(m.copy())
+    modtmp.add_metabolites(newM)
+
+    m = modtmp.metabolites.get_by_id(metabolite)
+    modtmp.objective = modtmp.add_boundary(m, type='demand')
+    sol = modtmp.slim_optimize()
+    if round(sol,12) > 0:
+        print m.id, m.name, "can be produced:", sol
+    else:
+        print m.id, m.name, "CANNOT be produced:", sol
+        print "\t --> try to find reactions for gapfilling"
+        #print gapfill(modtmp, refmod)   
+        filler = GapFiller(modtmp, refmod, integer_threshold=1e-32).fill()
+        print filler
+        for r in filler[0]:
+            print "\t", r.id, r.reaction
+        return filler
+
+def checkBiomass(model, fill_gaps=True):
+    modmin = set_medium(model, minmed)
+    if fill_gaps:
+        modnew = model.copy() # will be returned
+    newM = []
+    for m in refmod.metabolites:
+        if m not in modmin.metabolites:
+            newM.append(m.copy())
+    # add all metabolites from reference (needed for gapfilling => otherwise reactions sometimes not found!)
+    modmin.add_metabolites(newM) 
+    
+    Call=0; Cwork=0; Cfix=0
+    fixLater = []
+    fail = []
+    #relMet = ["cpd00288_c0"]
+    allMet = model.reactions.bio1.reactants #+ [modmin.metabolites.get_by_id(m) for m in relMet]
+    #allMet = model.reactions.Biomass.reactants # vmh
+    #bioaa = [m for m in model.metabolites if m.name.startswith("L_") and m in model.reactions.bio1.metabolites]
+    #for m in bioaa:
+    for m in allMet:
+        if m.formula in ignore: # ignore minerals, etc
+            continue
+        Call += 1
+        modtmp = modmin.copy()
+        modtmp.objective = modtmp.add_boundary(m, type='demand')
+        obj = modtmp.slim_optimize()
+        if obj > 0:
+            #print "\t => could be produced:"
+            Cwork += 1
+        elif fill_gaps: # gapfilling
+            print obj, m.id, m.name
+            try:
+                #gapsol = gapfill(modtmp, refmod) # integer_threshold=1e-06
+                gapsol = GapFiller(modtmp, refmod, integer_threshold=1e-16).fill()
+            except RuntimeError:
+                print "\t => Runtime error: lowering the integer_threshold?"
+                fixLater.append(m)
+                continue
+            except:
+                print "\t => failed:", sys.exc_info()[0]
+                fail.append(m)
+                continue
+            if len(gapsol[0]) > 0:
+                Cfix += 1
+                print "\t => could be fixed:", ",".join([r.id for r in gapsol[0]])
+                modnew.add_reactions([r for r in gapsol[0] if r not in modnew.reactions])
+    print "\nTotal compounds:", Call, "\t can be produced:", Cwork, "\t could be fixed", Cfix, "\t altogether:", round(100*float(Cwork+Cfix)/Call,1), "%", " (before:",round(100*float(Cwork)/Call,1),"% )"
+    if fill_gaps:
+        return(modnew)
+
+
+modnew = checkBiomass(mod, fill_gaps=True)
+fileID = os.path.splitext(os.path.basename(sys.argv[1]))[0]
+write_sbml_model(modnew, filename=fileID+"_gapfilled.xml")
---
+++ src/gapfill_reference.py
@@ -0,0 +1,145 @@
+def correct_mass(dic): # how to handle wrong charge????
+    if dic == {}:
+        return True
+    else:
+        return False
+
+def only_missing_H(dic):
+    if len(dic)==1 and "H" in dic:
+        return True
+    else:
+        return False
+
+def repair_mass_balance(model, delete_unbalanced=True, verbose=False):
+    mod = model.copy()
+    Cwrong = 0
+    Ccorrected = 0
+    if "cpd00067_c0" not in mod.metabolites:
+        h = Metabolite(id="cpd00067_c0", name="H+", formula="H", charge=1)
+        mod.add_metabolites(h)
+    
+    rea_rm = []
+    
+    mets = [m for m in mod.metabolites if m.formula==""]
+    for m in mets:
+        mid =re.sub("_.*$","", m.id)
+        hit = seedmDB.loc[seedmDB["id"]==mid]
+        m.formula = hit["formula"].values[0]
+        m.charge = hit["charge"].values[0]
+    print len(mets), "compounds without formula, fixed from total", len(mod.metabolites) 
+    
+    for r in mod.reactions:
+        if r in mod.exchanges or r.id=="bio1":
+            continue
+        
+        mbal = r.check_mass_balance()
+        if "charge" in mbal:
+            mbal.pop("charge") # ToDo: how to handle charge? (is not always in agreement with H!)
+        if not correct_mass(mbal): 
+            if verbose:
+                print r.id, r.reaction
+            Cwrong += 1
+            if not only_missing_H(mbal):
+                for m in r.metabolites: # check if one substance causes miss balance => remove
+                    tmp = r.copy()
+                    coef = tmp.get_coefficient(m.id)
+                    #print m.id, coef
+                    tmp.subtract_metabolites({m:coef})
+                    mbal2 = tmp.check_mass_balance()
+                    if "charge" in mbal2:
+                        mbal2.pop("charge")
+                    if only_missing_H(mbal2):
+                        if verbose:
+                            print "removed", m.id,m.name, "from", r.id
+                            print mbal, mbal2
+                        mbal = mbal2
+                        r.subtract_metabolites({m:-1})
+                        break
+            if only_missing_H(mbal): # check again if corrections could work
+                miss = mbal["H"]
+            else:
+                if verbose:
+                    print "could not be fixed"
+                    print "\t",r.id, r.name, r.reaction
+                    print "\t",mbal
+                if delete_unbalanced:
+                    rea_rm.append(r)
+                    #mod.remove_reactions(r)
+                continue
+            if miss != round(miss): # check if integer
+                print "error in", r.id, "mass imbalance:", mbal
+                if delete_unbalanced:
+                    rea_rm.append(r)
+                    #mod.remove_reactions(r)
+                continue
+            h = mod.metabolites.get_by_id("cpd00067_c0")
+            if miss < 0:
+                r.add_metabolites({h: -miss})
+            if miss > 0:
+                r.subtract_metabolites({h: miss})
+            #print mbal, r.check_mass_balance()
+            mbal3 = r.check_mass_balance()
+            if "charge" in mbal3:
+                mbal3.pop("charge")
+            if not correct_mass(mbal3):
+                if verbose:
+                    print r.id, r.name, r.reaction
+                    print "old:",mbal, "\tnew:",mbal3
+                if delete_unbalanced:
+                    rea_rm.append(r)
+                    #mod.remove_reactions(r)
+            else:
+                if verbose:
+                    print r.id, "fixed"
+                Ccorrected += 1
+    mod.remove_reactions(rea_rm)
+    #print ",".join([r.name for r in rea_rm])
+    #print [r.check_mass_balance() for r in rea_rm]
+    mod.repair()
+    print "Unbalanced reactions:", Cwrong, "\tCould be corrected:", Ccorrected, "\tRemoved reactions:", len(model.reactions)-len(mod.reactions)
+    return(mod)
+
+def get_reference(mod, newR, delete_unbalanced, verbose):
+    from cobra import Model, Reaction, Metabolite
+    import pandas
+    import re
+    seedrDB = pandas.read_csv("/home/jo/uni/gapseq/dat/seed_reactions.tsv", sep="\t")
+    seedmDB = pandas.read_csv("/home/jo/uni/gapseq/dat/seed_metabolites.tsv", sep="\t")
+
+    #refdb  = seedrDB.loc[seedrDB['abbreviation'].isin(newR)] # vmh
+    refdb  = seedrDB.loc[seedrDB['id'].isin(newR)]
+    refmod = Model("reaction_database")
+    Rmod   = [re.sub("_.*$", "", r.id) for r in mod.reactions]
+    print "Consider", len(refdb.index), "reactions"
+    Cold = 0
+    Calready = 0
+    for i,row in refdb.iterrows():
+        #rid = row["abbreviation"] # vmh
+        rid = row["id"]
+        if row["is_obsolete"] == 1: # do not add old reactions
+            Cold += 1
+            continue
+        #if rid in Rmod: # vmh
+        elif rid in Rmod:
+            Calready += 1
+            continue
+        r = Reaction(rid+"_c0") # default seed model have compartment tag
+        refmod.add_reaction(r)
+        #rstr = row["formula"] # vmh
+        rstr = row["equation"]
+        #rstr = row["code"]
+        rstr = rstr.replace("[0]", "_c0").replace("[1]", "_e0").replace("[3]", "_p0")
+        r.build_reaction_from_string(rstr, verbose=False)
+        #r.reaction = rstr
+    for m in refmod.metabolites:
+        mid =re.sub("_.*$","", m.id)
+        hit = seedmDB.loc[seedmDB["id"]==mid]
+        m.name = hit["name"].values[0]
+        m.formula = hit["formula"].values[0]
+        m.charge = hit["charge"].values[0]
+
+    print Calready, "reactions already in the model"
+    print Cold, "removed deprecated reactions"
+    refmod = repair_mass_balance(refmod, delete_unbalanced, verbose)
+    print len(refmod.reactions), "remaining reaction in reference database:", 
+    return(refmod)
