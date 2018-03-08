81ac367e80abb6309cd306c975261dfd43fa5a0f
jo <jotech@users.noreply.github.com>
2018-03-08 17:17:05 +0100

    updated metacyc reaction database
    - reaction database from pathwaytools 21.5
    - show if reaction is enzymatic
    - direction of reaction
    - gibbs energy change

--- src/meta2kegg.py
+++ src/meta2kegg.py
@@ -9,8 +9,8 @@ pythoncyc.sendQueryToPTools("(select-organism :org-id 'META)")
 
 
 
-ofile = open("./meta_rea.csv", "w")
-ofile.write("id" + "\t" + "name" + "\t" + "altname" + "\t" + "ec" + "\t" + "kegg" + "\t" + "pwy" + "\n")
+ofile = open("./meta_rea.tbl", "w")
+ofile.write("id" + "\t" + "name" + "\t" + "altname" + "\t" + "ec" + "\t" + "kegg" + "\t" + "pwy" + "\t" + "enzymatic" + "\t" + "gibbs" + "\t" + "direction" + "\n")
 for r in meta.all_reactions():
     rea = meta[r]
     #qry = "(get-instance-all-types '"+p+")"
@@ -25,6 +25,8 @@ for r in meta.all_reactions():
         altname = ""
     if rea.ec_number != None:
         ec = ",".join([ec.replace("|","") for ec in rea.ec_number])
+    else:
+        ec = ""
     kegghit = re.findall("R[0-9]{5}",str(rea.dblinks))
     if kegghit != []:
         kegg = ",".join(kegghit)
@@ -32,6 +34,21 @@ for r in meta.all_reactions():
         kegg = ""
     if rea.in_pathway != None:
         pwy = ",".join([p.replace("|","") for p in rea.in_pathway])
-    ofile.write(r + "\t" + name + "\t" + altname + "\t" + ec + "\t" + kegg + "\t" + pwy + "\n")
+    else:
+        pwy = ""
+    if rea.enzymatic_reaction != None and len(rea.enzymatic_reaction) > 0:
+        enzymatic = True
+    else:
+        enzymatic = False
+    if rea.gibbs_0 != None:
+        gibbs = rea.gibbs_0
+    else:
+        gibbs = ""
+    if rea.reaction_direction != None:
+        dir_dic = {"|PHYSIOL-LEFT-TO-RIGHT|":">", "|PHYSIOL-RIGHT-TO-LEFT|":"<", "|LEFT-TO-RIGHT|":">", "|RIGHT-TO-LEFT|":"<", "|REVERSIBLE|":"=", "|IRREVERSIBLE-RIGHT-TO-LEFT|":"<", "|IRREVERSIBLE-LEFT-TO-RIGHT|":">"}
+        direction = dir_dic[rea.reaction_direction]
+    else:
+        direction = "?"
+    ofile.write(r + "\t" + name + "\t" + altname + "\t" + ec + "\t" + kegg + "\t" + pwy + "\t" + str(enzymatic) + "\t" + str(gibbs) + "\t" + direction + "\n")
 ofile.close()
 
