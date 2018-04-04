fdec8ac6053f8baffb2c0eeaa5f7c167ff0e0b27
Waschina <s.waschina@posteo.net>
2018-04-04 18:23:58 +0200

    R-script, which constructs a full model(*) from reaction database
    (*) A network model (sybil::modelorg) that contains all reactions
    that are listed in the database.
    Following columns of the database-file a used:
    - id
    - reversibility
    - stoichiometry

---
+++ src/construct_full_model.R
@@ -0,0 +1,65 @@
+construct_full_model <- function(script.path) {
+  require(data.table)
+  require(stringr)
+  #require(sybilSBML)
+  require(sybil)
+  source(paste0(script.dir, "/add_missing_exRxns.R"))
+  
+  mseed <- fread(paste0(script.dir, "/../dat/seed_reactions_corrected.tsv"), header=T, stringsAsFactors = F)
+  mseed <- mseed[gapseq.status %in% c("approved","corrected")]
+  mseed <- mseed[order(id)]
+  
+  mod <- modelorg(name = "Full Dummy model with all approved/corrected ModelSEED reactions",id = "dummy")
+  for(i in (1:nrow(mseed))) {
+    cat(paste0("\r",i))
+    mets  <- unlist(str_split(string = mseed[i,compound_ids],pattern = ";"))
+    rxn.info <- str_split(unlist(str_split(string = mseed[i,stoichiometry],pattern = ";")), pattern = ":", simplify = T)
+    
+    met.comp  <- rxn.info[,3]
+    met.comp.n <- ifelse(met.comp==0,"c0","e0")
+    met.comp.n <- ifelse(met.comp>=2,"p0",met.comp.n)
+    
+    met.ids   <- paste0(rxn.info[,2],"[",met.comp.n,"]")
+    met.scoef <- as.numeric(rxn.info[,1])
+    met.name  <- str_replace_all(rxn.info[,5],"\\\"","")
+    
+    met.name  <- paste0(met.name,"-",met.comp.n)
+    
+    ind <- which(met.name=="" | is.na(met.name))
+    met.name[ind] <- met.ids[ind]
+    
+    is.rev <- ifelse(mseed[i,reversibility] %in% c("<","="),T,F)
+    only.backwards <- ifelse(mseed[i,reversibility]=="<",T,F)
+    
+    ind.new.mets <- which(met.ids %in% mod@met_id)
+    ind.old.mets <- which(mod@met_id %in% met.ids[ind.new.mets])
+    
+    met.name[ind.new.mets] <- mod@met_name[ind.old.mets]
+    
+    mod <- addReact(model = mod, 
+                    id = paste0(mseed[i,id],"_c0"), 
+                    met = met.ids,
+                    Scoef = met.scoef,
+                    reversible = is.rev, 
+                    metComp = as.integer(met.comp)+1,
+                    ub = ifelse(only.backwards, 0, 1000),
+                    lb = ifelse(is.rev, -1000, 0),
+                    reactName = mseed[i, name], 
+                    metName = met.name)
+  }
+  cat("\n")
+  mod <- add_missing_exchanges(mod) 
+  saveRDS(mod,file = paste0(script.dir, "/../dat/full.model.RDS"))  
+}
+
+# get current script path
+if (!is.na(Sys.getenv("RSTUDIO", unset = NA))) {
+  # RStudio specific code
+  script.dir    <- dirname(rstudioapi::getSourceEditorContext()$path)
+} else{
+  initial.options <- commandArgs(trailingOnly = FALSE)
+  script.name <- sub("--file=", "", initial.options[grep("--file=", initial.options)])
+  script.dir  <- dirname(script.name)
+}
+
+construct_full_model(script.path = script.dir)
