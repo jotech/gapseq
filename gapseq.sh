130c87f6e429abb5191c3b8f0da3937c04d33209
jo <jotech@users.noreply.github.com>
2018-07-18 16:26:39 +0200

    better parsing for exception reactions
    - fixed grep search for special chars (e.g. square brackets)

--- gapseq.sh
+++ gapseq.sh
@@ -285,10 +285,9 @@ do
         reaName=$(echo $reaNames | awk -v j=$j -F ';' '{print $j}' | tr -d '|')
         re="([0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+)"
         EC_test=$(if [[ $ec =~ $re ]]; then echo ${BASH_REMATCH[1]}; fi) # check if not trunked ec number (=> too many hits)
-        [[ -n "$ec" ]] && [[ -n "$reaName" ]] && { ex_pattern="$ec$|$reaName"; }
-        [[ -z "$ec" ]] && [[ -n "$reaName" ]] && { ex_pattern="$reaName"; }
-        [[ -n "$ec" ]] && [[ -z "$reaName" ]] && { ex_pattern="$ec"; }
-        is_exception=$(grep -Ew "$ex_pattern" $dir/dat/exception.tbl | wc -l)
+        [[ -n "$ec" ]] && [[ -n "$reaName" ]] && { is_exception=$(grep -F -e "$ec" -e "$reaName" $dir/dat/exception.tbl | wc -l); }
+        [[ -z "$ec" ]] && [[ -n "$reaName" ]] && { is_exception=$(grep -F "$reaName" $dir/dat/exception.tbl | wc -l); }
+        [[ -n "$ec" ]] && [[ -z "$reaName" ]] && { is_exception=$(grep -F "$ec" $dir/dat/exception.tbl | wc -l); }
         if [[ $is_exception -gt 0 ]] && [[ $identcutoff -lt 70 ]];then # take care of similair enzymes with different function
             identcutoff_tmp=70
             echo -e "\tUsing higher identity cutoff for $rea"
