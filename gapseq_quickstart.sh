e9b920f041f03ec6704cdc4ba923bc36aa749d1c
Waschina <s.waschina@posteo.net>
2018-11-21 14:16:00 +0100

    A script to quickly reconstruct model with default parametrisation

---
+++ gapseq_quickstart.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+fasta=$1
+model="${fasta/.fna*/}"
+media=$2
+
+path=$(readlink -f "$0")
+dir=$(dirname "$path")
+
+$dir/./gapseq.sh -b 200 -p all $1
+
+$dir/./transporter.sh $1
+
+Rscript $dir/src/generate_GSdraft.R -r $model-all-Reactions.tbl -t "$model-Transporter.lst" -c $fasta -b 200
+
+Rscript $dir/gf.suite.R -m $model.RDS -n $media -c $model-rxnWeights.RDS -b 50
