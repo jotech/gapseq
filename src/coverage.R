c05c678c22194d88abe109ee1f6f6fc72eec683d
jo <jotech@users.noreply.github.com>
2020-01-29 14:29:00 +0100

    annotation genome coverage with transporter
    - extended annotation genome coverage calculation to include additional transporter file
    - Rscript src/coverage.R toy/ecoli.fna.gz toy/ecoli-all-Reactions.tbl toy/ecoli-Transporter.tbl

--- src/coverage.R
+++ src/coverage.R
@@ -4,7 +4,7 @@
 # second argument: annotation file
 
 args = commandArgs(trailingOnly=TRUE)
-if (length(args)!=2) {
+if ( length(args)!=2 & length(args)!=3 ) {
   stop("First argument should be a genome file and the second one an gapseq reaction file.")
 }
 
@@ -12,12 +12,17 @@ suppressMessages(library(data.table))
 suppressMessages(library(Biostrings))
 suppressMessages(library(IRanges))
 
-coverage <- function(genome.filestr, anno.filestr){
+coverage <- function(genome.filestr, anno.filestr, anno.filestr2=NA){
   #genome <- readDNAStringSet("~/uni/gapseq/toy/ecoli.fna.gz")
   #anno   <- fread("~/uni/gapseq/toy/ecoli-all-Reactions.tbl", fill = T)
   genome   <- readDNAStringSet(genome.filestr)
   anno.raw <- fread(anno.filestr, fill = T)
   anno     <- unique(anno.raw[,.(sstart, send, stitle)])
+  if( !is.na(anno.filestr2) ){
+    anno.raw2<- fread(anno.filestr2, fill = T)  
+    anno2    <- unique(anno.raw2[,.(sstart, send, stitle)])
+    anno     <- rbind(anno, anno2)
+  }
   anno <- anno[!is.na(sstart) & !is.na(send)]
   anno[,start:=ifelse(sstart<send, sstart, send)]
   anno[,end:=ifelse(send>sstart, send, sstart)]
@@ -48,5 +53,11 @@ coverage <- function(genome.filestr, anno.filestr){
   
   return(invisible(genome.cov))
 }
+#coverage("~/uni/gapseq/toy/ecoli.fna.gz", "~/uni/gapseq/toy/ecoli-all-Reactions.tbl", "~/uni/gapseq/toy/ecoli-Transporter.tbl") # 0.342
+#coverage("~/uni/gapseq/toy/myb71.fna.gz", "~/uni/gapseq/toy/myb71-all-Reactions.tbl", "~/uni/gapseq/toy/myb71-Transporter.tbl") # 0.2643
 
-coverage(args[1], args[2])
+if( length(args) == 2){
+  coverage(args[1], args[2])  
+}else{
+  coverage(args[1], args[2], args[3])
+}
