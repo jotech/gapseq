a209ec705cc41acbd1efdef3f32cd3986490c61c
jotech <jo@jotech.net>
2021-05-05 12:25:55 +0200

    repaired gram prediction by network
    - update database with reference networks
    - corrected errors in script

--- src/gram_by_network.R
+++ src/gram_by_network.R
@@ -8,19 +8,16 @@ find_gram_by_network <- function(file, script.dir){
   ref.info <- fread(paste0(script.dir, "/../dat/", "reference_genomes_edited.tbl"))
   
   dt.new <- fread(file)
-  dt.new$Prediction <- as.numeric(as.logical(dt.new$Prediction))
+  dt.new[,Prediction:=Prediction*1]
   org    <- gsub("-all-Pathways.tbl","",basename(file))
-  dt.new$org <- org
   
-  dcast.new<- dcast.data.table(dt.new, formula=org~ID, value.var = "Prediction")
-  dcast.new<- dcast.new[1,]
-  idx.new <- which(colnames(dcast.new) %in% colnames(dcast.gapseq))
-  idx.old <- which(colnames(dcast.gapseq) %in% colnames(dcast.new))
-  dcast.all <- rbind(dcast.gapseq[,..idx.old],dcast.new[,..idx.new])
-  mat.gapseq   <- as.matrix(dcast.all[,-1])
-  
-  rownames(mat.gapseq) <- dcast.all$org
-  
+  dcast.new <- matrix(nrow=1, ncol=nrow(dt.new), data=dt.new$Prediction); colnames(dcast.new) <- dt.new$ID
+  #dcast.new<- dcast.data.table(unique(dt.new), formula=~ID, value.var = "Prediction")
+  comm.pwy <- intersect(colnames(dcast.new), colnames(dcast.gapseq))
+  idx.old <- match(comm.pwy, colnames(dcast.gapseq))
+  idx.new <- match(comm.pwy, colnames(dcast.new))
+  mat.gapseq <- as.matrix(rbind(dcast.gapseq[,c(idx.old)],dcast.new[,c(idx.new)]))
+  rownames(mat.gapseq)[nrow(mat.gapseq)] <- org
   
   dist.gapseq <- dist(mat.gapseq)
   
@@ -29,7 +26,7 @@ find_gram_by_network <- function(file, script.dir){
   dist.mat[idx,idx] <- Inf # tmp hack
   closest <- which.min(dist.mat[,idx])
   
-  hit <- ref.info[make.names(name)==rownames(dist.mat)[closest],.(name,gram)]
+  hit <- ref.info[name==rownames(dist.mat)[closest],.(name,gram)]
   
   cat("Closest metabolic network:", hit$name, "\tgram:", hit$gram, "\n")  
   return(hit$gram)
