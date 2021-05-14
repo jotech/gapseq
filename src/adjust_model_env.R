41759c61c05118d2f7fa79dd341d6e0f4ceff214
Waschina <s.waschina@posteo.net>
2021-05-14 15:32:32 +0200

    Bug fix (Environment adjustment)
    - An error occurred if none of the reactions, whose are changed in an
    environment adjustment, are part of the models (empty data.table).

--- src/adjust_model_env.R
+++ src/adjust_model_env.R
@@ -3,13 +3,15 @@ adjust_model_env <- function(mod, env, script.dir) {
     env_dt <- fread(paste0(script.dir, "/../dat/env/env_highH2.tsv"), header = F)
     env_dt[, V1 := paste0(V1,"_c0")]
     env_dt <- env_dt[V1 %in% mod@react_id]
-    for(i in 1:nrow(env_dt)) {
-      if(env_dt[i, V2] == ">")
-        mod <- changeBounds(mod, react = env_dt[i, V1], lb = 0, ub = sybil::SYBIL_SETTINGS("MAXIMUM"))
-      if(env_dt[i, V2] == "<")
-        mod <- changeBounds(mod, react = env_dt[i, V1], lb = -sybil::SYBIL_SETTINGS("MAXIMUM"), ub = 0)
-      if(env_dt[i, V2] == "=")
-        mod <- changeBounds(mod, react = env_dt[i, V1], lb = -sybil::SYBIL_SETTINGS("MAXIMUM"), ub = sybil::SYBIL_SETTINGS("MAXIMUM"))
+    if(nrow(env_dt) > 0) {
+      for(i in 1:nrow(env_dt)) {
+        if(env_dt[i, V2] == ">")
+          mod <- changeBounds(mod, react = env_dt[i, V1], lb = 0, ub = sybil::SYBIL_SETTINGS("MAXIMUM"))
+        if(env_dt[i, V2] == "<")
+          mod <- changeBounds(mod, react = env_dt[i, V1], lb = -sybil::SYBIL_SETTINGS("MAXIMUM"), ub = 0)
+        if(env_dt[i, V2] == "=")
+          mod <- changeBounds(mod, react = env_dt[i, V1], lb = -sybil::SYBIL_SETTINGS("MAXIMUM"), ub = sybil::SYBIL_SETTINGS("MAXIMUM"))
+      }
     }
   }
   
