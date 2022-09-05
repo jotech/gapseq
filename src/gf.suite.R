jotech/gapseq

commit c1e35b7cc54aed72bf95bfd536901cd03e8c6295

check for model attributes beforehand
- quick fix for #133

src/gf.suite.R
@@ -157,7 +157,7 @@ if(env[1] != "")
   mod <- adjust_model_env(mod, env, script.dir)
 
 # block reactions in domain, which do not have these reactions
-if(any(grepl("tax_domain:", mod.orig@mod_attr[,"annotation"], fixed = T))) {
+if(any(grepl("tax_domain:", mod.orig@mod_attr, fixed = T))) {
   domain.rxn.exclusions <- fread(paste0(script.dir, "/../dat/biomass/excluded_reactions.tsv"), header=T, stringsAsFactors = F)
   
   anno_ind   <- which(grepl("tax_domain:", mod.orig@mod_attr[,"annotation"], fixed = T))
@@ -674,7 +674,7 @@ mod.out <- rm_unused_exchanges(mod.out)
 
 # add metabolite-, reaction-, and model attributes
 mod.out <- addMetAttr(mod.out, seed_x_mets = seed_x_mets)
-mod.out <- addReactAttr(mod.out)
+if("ec" %in% colnames(mod.out@react_attr)) mod.out <- addReactAttr(mod.out)
 mod.out@mod_attr <- bu_mod_attr
 
 if(!dir.exists(output.dir))

commit 82a28b71cf4d2528f5ae215665958c59c4552d33

Fix of bug introduced in c1e35b7cc54aed72bf95bfd536901cd03e8c6295

src/gf.suite.R
@@ -152,12 +152,25 @@ if ( toupper(file_ext(mod.file)) == "RDS" ){
 
 bu_mod_attr <- mod.orig@mod_attr
 
+# Add annotation column to model attributes if not already there
+if(!("annotation" %in% colnames(mod.orig@mod_attr))) {
+  bm_ind <- which(mod.orig@react_id == "bio1")
+  annostr <- ""
+  if(grepl("Bacteria",mod.orig@react_name[bm_ind]))
+    annostr <- "tax_domain:Bacteria"
+  if(grepl("Archaea",mod.orig@react_name[bm_ind]))
+    annostr <- "tax_domain:Archaea"
+  
+  mod.orig@mod_attr <- cbind(mod.orig@mod_attr,
+                             data.frame(annotation = annostr))
+}
+
 # adjust environment if needed
 if(env[1] != "")
   mod <- adjust_model_env(mod, env, script.dir)
 
 # block reactions in domain, which do not have these reactions
-if(any(grepl("tax_domain:", mod.orig@mod_attr, fixed = T))) {
+if(any(grepl("tax_domain:", mod.orig@mod_attr[,"annotation"], fixed = T))) {
   domain.rxn.exclusions <- fread(paste0(script.dir, "/../dat/biomass/excluded_reactions.tsv"), header=T, stringsAsFactors = F)
   
   anno_ind   <- which(grepl("tax_domain:", mod.orig@mod_attr[,"annotation"], fixed = T))