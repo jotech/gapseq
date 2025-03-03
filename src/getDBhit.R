6cde2ef5f667000584bb9691ed11c68f9e065ce4
Silvio Waschina <s.waschina@nutrinf.uni-kiel.de>
2025-03-03 15:06:49 +0100

    getDBhit implementation in R

--- src/getDBhit.R
+++ src/getDBhit.R
@@ -1,4 +1,6 @@
-# Get database hits for reaction queries (by EC and/or name)
+# Get gapseq reaction database hits for reaction queries by EC and/or name. The
+# function also identifies alternative ECs (for later reference sequence
+# collection).
 #
 # Arguments:
 # `rea` - Reaction ID in query data source (in most cases: metacyc rection ID)
@@ -6,8 +8,12 @@
 # `ec` - Enzyme Commission (EC) number
 # `database` - Target database. "seed" or "vmh" (Support for vmh might be stopped soon)
 #
-getDBhit(rea, reaName, ec, database) {
-  # Check if EC number is complete and valid
+getDBhit <- function(rea, reaName, ec, database) {
+  dbhit <- c()
+
+  ec <- unique(str_split(ec, "/")[[1]])
+
+  # Check if EC numbers are complete and valid
   EC_test <- grepl("^[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+$",ec)
 
   # EC with escaped points
@@ -16,12 +22,12 @@ getDBhit(rea, reaName, ec, database) {
   # get KEGG ID(s) for metacyc reaction (comma-separated)
   kegg <- metaRea[id == paste0("|",rea,"|"), kegg]
 
-  # 1) search in target reaction DB by query EC
-  if(EC_test) {
+  # 1) search in target reaction DB by query EC (works with multiple EC input)
+  if(any(EC_test)) {
     if(database == "vmh") {
-      dbhit <- reaDB1[grepl(paste0(ec_esc,"($| |,|;)"), ecnumber), abbreviation]
+      dbhit <- reaDB1[grepl(paste0(ec_esc[EC_test],"($| |,|;)", collapse = "|"), ecnumber), abbreviation]
     } else if(database == "seed") {
-      dbhit <- seedEC[grepl(paste0(ec_esc,"$"), `External ID`), `MS ID`]
+      dbhit <- seedEC[grepl(paste0(ec_esc[EC_test],"$", collapse = "|"), `External ID`), `MS ID`]
       dbhit <- unlist(strsplit(dbhit, "\\|"))
     }
   }
@@ -32,34 +38,46 @@ getDBhit(rea, reaName, ec, database) {
       hittmp <- reaDB1[grepl(gsub(",","|",kegg), keggId), abbreviation]
       dbhit <- c(dbhit, hittmp)
     } else if(database == "seed") {
-      hittmp <- seedEC[grepl(paste0(ec_esc,"$"), `External ID`), `MS ID`]
+      hittmp <- reaDB5[grepl(gsub(",","|",kegg), other), seed]
       hittmp <- unlist(strsplit(hittmp, "\\|"))
       dbhit <- c(dbhit, hittmp)
     }
   }
 
   # 3) search in target reaction DB by query alternative EC
-  if(EC_test) {
-    altec <- altecdb[grepl(paste0(ec_esc,"($|,)"),altecdb)]
-    altec <- unlist(strsplit(altec,","))
-    altec <- altec[altec != ec]
+  altec <- character(0L)
+  altec_src <- character(0L)
+  if(sum(EC_test) > 0) {
+    for(i in 1:length(ec)) {
+      if(EC_test[i]) {
+        altectmp <- altecdb[grepl(paste0(ec_esc[i],"($|,)"),altecdb)]
+        altectmp <- unlist(strsplit(altectmp,","))
+        altectmp <- altectmp[altectmp %notin% ec]
+        altec_srctmp <- rep("altec.csv", length(altectmp))
 
-    if(is.null(altec)) {
-      brendaec <- brenda[grepl(paste0(ec_esc,"($|,| |\t|;)"),brenda)]
-      brendaec <- paste(brendaec, collapse = ";")
-      brendaec <- str_extract_all(brendaec, "[0-9]\\.[0-9]+\\.[0-9]+\\.[0-9]+(?=$|,|;|\\.)")[[1]]
-      altec <- unique(brendaec[brendaec != ec])
-    }
+        if(is.null(altectmp)) {
+          brendaec <- brenda[grepl(paste0(ec_esc[i],"($|,| |\t|;)"),brenda)]
+          brendaec <- paste(brendaec, collapse = ";")
+          brendaec <- str_extract_all(brendaec, "[0-9]\\.[0-9]+\\.[0-9]+\\.[0-9]+(?=$|,|;|\\.)")[[1]]
+          altectmp <- unique(brendaec[brendaec %notin% ec])
+          altec_srctmp <- rep("brenda", length(altectmp))
+        }
 
-    if(length(altec) > 0) {
-      altec_esc <- gsub(".","\\.", altec, fixed = TRUE)
-      altec_esc_comb <- paste0(altec_esc,"(,|$|;)", collapse = "|")
-      if(database == "vmh") {
-        hittmp <- reaDB1[grepl(altec_esc_comb, ecnumber), abbreviation]
-        dbhit <- c(dbhit, hittmp)
-      } else if(database == "seed") {
-        hittmp <- seedEC[`External ID` %in% altec, `MS ID`]
-        dbhit <- c(dbhit, hittmp)
+        altec <- c(altec, altectmp)
+        altec_src <- c(altec_src, altec_srctmp)
+
+        if(length(altectmp) > 0) {
+          altec_esc <- gsub(".","\\.", altectmp, fixed = TRUE)
+          altec_esc_comb <- paste0(altec_esc,"(,|$|;)", collapse = "|")
+          if(database == "vmh") {
+            hittmp <- reaDB1[grepl(altec_esc_comb, ecnumber), abbreviation]
+            dbhit <- c(dbhit, hittmp)
+          } else if(database == "seed") {
+            hittmp <- seedEC[`External ID` %in% altectmp, `MS ID`]
+            hittmp <- unlist(strsplit(hittmp, "\\|"))
+            dbhit <- c(dbhit, hittmp)
+          }
+        }
       }
     }
   }
@@ -85,8 +103,9 @@ getDBhit(rea, reaName, ec, database) {
     dbhit <- c(dbhit, hittmp)
   }
 
-  return(list(dbhit = order(unique(dbhit)),
-              altec = altec))
+  return(list(dbhit = paste(sort(unique(dbhit)), collapse = " "),
+              altec = paste(altec, collapse = ","),
+              altec_src = paste(altec_src, collapse = ",")))
 }
 
 
