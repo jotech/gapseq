58ef3e7d0a3ad861eb1348854b420835e96c9d53
Silvio Waschina <s.waschina@nutrinf.uni-kiel.de>
2025-03-04 15:07:44 +0100

    More steps towards batch alignments

--- src/prepare_batch_alignments.R
+++ src/prepare_batch_alignments.R
@@ -23,6 +23,7 @@ pwyDB <- fread(args[1])
 setkey(pwyDB, "V1")
 database <- args[2]
 taxonomy <- args[3]
+seqSrc <- args[4]
 
 saveRDS(pwyDB, "~/tmp/pwyDB.RDS")
 # pwyDB <- readRDS("~/tmp/pwyDB.RDS")
@@ -58,10 +59,17 @@ reaec <- pwyrea[, getDBhit(rea, reaName, ec, database), by = .(rea, reaName, ec)
 # collate reference sequences to use for later alignments
 #-------------------------------------------------------------------------------
 
-allseqfiles <- dir(paste0(script.dir,"/../dat/seq/",taxonomy,"/"),
-                   recursive = TRUE,
-                   pattern = "\\.fasta$")
-fsz <- file.size(paste0(script.dir,"/../dat/seq/",taxonomy,"/",allseqfiles))
+# allseqfiles <- dir(paste0(script.dir,"/../dat/seq/",taxonomy,"/"),
+#                    recursive = TRUE,
+#                    pattern = "\\.fasta$")
+# fsz <- file.size(paste0(script.dir,"/../dat/seq/",taxonomy,"/",allseqfiles))
+# names(fsz) <- allseqfiles
+
+# for some metacyc reactions, there are already uniprot accessions assigned.
+metaGenes <- fread(paste0(script.dir,"/../dat/meta_genes.csv"))
+metaGenes[, rxn := gsub("^\\||\\|$","",rxn)]
+setkey(metaGenes, "rxn")
+metaGenes <- metaGenes[uniprot != ""]
 
 md5sum <- function(str) {
   temp_file <- tempfile()
@@ -75,14 +83,15 @@ identifySeqFiles <- function(reaID, reaName, ecs, altecs) {
   allecs <- c(unlist(str_split(ecs, "/")),
               unlist(str_split(altecs, "/")))
   allecs <- allecs[grepl("^[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+$",allecs)]
+  rnmd5 <- md5sum(reaName)
 
   # MetaCyc geneXreaction assignments
-  sq_rxn <- paste0("rxn/",reaID,".fasta") # when is this not there?
+  sq_rxn <- paste0("rxn/",reaID,".fasta") # what when this one is not there?
 
   # geneXec assignments (reviewed and unreviewed)
   if(length(allecs) > 0) {
     sq_revEC <- paste0("rev/",allecs,".fasta")
-    sq_unrevEC <- paste0("rev/",allecs,".fasta")
+    sq_unrevEC <- paste0("unrev/",allecs,".fasta")
   } else {
     sq_revEC <- character(0L)
     sq_unrevEC <- character(0L)
@@ -90,18 +99,49 @@ identifySeqFiles <- function(reaID, reaName, ecs, altecs) {
 
   # geneXreaName assignments (reviewed and unreviewed)
   if(reaName != "") {
-    sq_revName <- paste0("rev/",md5sum(reaName),".fasta")
-    sq_unrevName <- paste0("unrev/",md5sum(reaName),".fasta")
+    sq_revName <- paste0("rev/",rnmd5,".fasta")
+    sq_unrevName <- paste0("unrev/",rnmd5,".fasta")
   } else {
     sq_revName <- character(0L)
     sq_unrevName <- character(0L)
   }
 
+  # user files
+  sq_user <- paste0("user/",
+                    c(allecs,rnmd5,reaID),
+                    ".fasta")
+
   fileDT <- data.table(file = c(sq_rxn,
                                 sq_revEC, sq_revName,
-                                sq_unrevEC, sq_unrevName),
+                                sq_unrevEC, sq_unrevName,
+                                sq_user),
                        type = c("metacyc",
                                 rep("EC",length(sq_revEC)), rep("reaName",length(sq_revName)),
-                                rep("EC",length(sq_revEC)), rep("reaName",length(sq_revName))))
+                                rep("EC",length(sq_revEC)), rep("reaName",length(sq_revName)),
+                                rep("EC",length(allecs)),"reaName","metacyc"))
   fileDT[, src := sub("/.*$","",file)]
+  fileDT[, use := FALSE]
+  fileDT[, fex := file.exists(paste0(script.dir,"/../dat/seq/",taxonomy,"/",file))]
+  fileDT <- fileDT[!(src == "user" & fex == FALSE)] # when user fasta does not exist, nothing to collect/use here
+  fileDT[fex == TRUE, file_size := file.size(paste0(script.dir,"/../dat/seq/",taxonomy,"/",file))]
+
+  # TODO: Check if something needs to be downloaded here
+  # Everything of type "EC" should be there
+  # files for hashed reaName should be there in cases where all EC files are empty
+  # files for "rxn" should be there, if the reaction ID is listed in table metaGenes
+
+  if(seqSrc == "1") {
+    # only reviewed
+
+  } else if(seqSrc == "2") {
+    # unreviewed only if reviewed not available
+
+  } else if(seqSrc == "3") {
+    # reviewed+unreviewed
+
+  } else if(seqSrc == "4") {
+    # only unreviewed
+
+  }
+
 }
