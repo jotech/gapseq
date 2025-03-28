687c1cffa351c288bd7ceaeca54672351e20ef78
Silvio Waschina <s.waschina@nutrinf.uni-kiel.de>
2025-03-28 17:10:55 +0100

    Analysing batch alignment statistics
    Not done yet...

--- src/analyse_alignments.R
+++ src/analyse_alignments.R
@@ -38,11 +38,35 @@ reaec
 seqfiles
 setnames(seqfiles,"ecs","ec")
 
-# identify reactions probably catalysed by protein complexes
-cplx <- merge(seqfiles, seq_headers, by = "file", allow.cartesian = TRUE)
-
+# identify reactions probably catalyzed by protein complexes
+cplx <- merge(seqfiles[use == TRUE, .(file, rea, reaName, ec)], seq_headers, by = "file", allow.cartesian = TRUE)
+cplx <- unique(cplx, by = c("rea","reaName","ec", "header"))
+cplx[, is_complex := any(grepl("subunit|chain|polypeptide|component",header)), by = .(rea, reaName, ec)]
+cplx[is_complex == TRUE, complex := complex_detection(header), by = .(rea, reaName, ec)]
+cplx[, subunit_count := length(unique(complex[!is.na(complex)])), by = .(rea, reaName, ec)]
+cplx <- cplx[subunit_count < 2, is_complex := FALSE]
+cplx[, qseqid := sub(" .*$","",header)]
+cplx[is_complex == TRUE & is.na(complex), complex := "Subunit undefined"]
+setkeyv(cplx, cols = c("rea","reaName","ec","qseqid"))
+cplx <- unique(cplx, by =  c("rea","reaName","ec","qseqid"))
+cplx_bin <- cplx[,.(is_complex = any(is_complex),
+                    subunit_count = subunit_count[1],
+                    subunits = paste(unique(complex),collapse = ",")), by = .(rea, reaName, ec)]
+cplx_bin[is_complex == FALSE, subunit_count := NA_integer_]
 
 #
-rxndt <- merge(pwyrea, reaec, by = c("rea", "reaName", "ec"), all.x = TRUE)
-rxndt <- merge(rxndt, seqfiles[use == TRUE, .(rea, reaName, ec, file, type, src)], by = c("rea","reaName", "ec"), all.x = TRUE)
+rxndt <- merge(reaec[,.(rxn = rea, name = reaName, ec, dbhit)],
+               seqfiles[use == TRUE, .(rxn = rea, name = reaName, ec, file, type, src)], by = c("rxn","name", "ec"),
+               all.x = TRUE)
+rxndt <- merge(rxndt, cplx_bin[,.(rxn = rea, name = reaName, ec, is_complex, subunit_count)], by = c("rxn","name", "ec"), all.x = TRUE)
 rxndt <- merge(rxndt, alignments, by = "file", all.x = TRUE, allow.cartesian = TRUE)
+rxndt$complex <- cplx[rxndt[,.(rxn, name, ec, qseqid)], complex]
+rxndt[is.na(is_complex), is_complex := FALSE]
+
+
+rxndt[!is.na(file) & is_complex == TRUE, complex := cplx[.(rxn, name, ec, qseqid),]]
+
+
+# rxndt <- merge(pwyrea, reaec, by = c("rea", "reaName", "ec"), all.x = TRUE)
+# rxndt <- merge(rxndt, seqfiles[use == TRUE, .(rea, reaName, ec, file, type, src)], by = c("rea","reaName", "ec"), all.x = TRUE)
+# rxndt <- merge(rxndt, alignments, by = "file", all.x = TRUE, allow.cartesian = TRUE)
